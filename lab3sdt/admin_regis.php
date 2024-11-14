83c53e33ebe64e585d9634880ba8f3510a5b13ed
Brendan Chia <148305613+Pegasus762@users.noreply.github.com>
2024-11-15 00:12:18 +0800

    Add files via upload

---
+++ lab3sdt/admin_regis.php
@@ -0,0 +1,67 @@
+<html>
+    <head>
+        <title>Admin Registration</title>
+        <link link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    </head>
+    
+    <body>
+        <!--Main container-->
+    <div class="container d-flex justify-content-center align-items-center  min-vh-100">
+    <!--Login container-->
+    <div class="row border rounded-5 p-3 bg-white shadow box-area">    
+  
+    <!--Left side-->
+<div class="col-md-6 rounded-4 d-flex justify-content-center align-items-center flex-column left box"
+    style="background:lightblue">
+      <div class="edu-image">
+        <img src="edu-image.jpg" class="image-fluid" style="width: 250px;">
+        <!--(image-fluid) images automatically scale to fit the width of their parent container-->
+      </div>
+</div>
+
+    <!--Right box-->
+<div class="col-md-6 right box">
+    <div class="row align-items-center">
+        <div class="header-text mb-4">
+            <p><b>Admin Registration<b></p>   
+        
+        <form action="admin_regis.php" method="POST">
+            <div class="input-group mb-3">
+                    <input type="text" class="form-control form-control-lg bg-light fs-6" placeholder="Username">
+                </div>
+                <div class="input-group mb-1">
+                    <input type="password" class="form-control form-control-lg bg-light fs-6" placeholder="Password">
+                </div>
+        
+                <div class="input-group mb-5 d-flex justify-content-between">
+                    <div class="form-check">
+                        <input type="checkbox" class="form-check-input" id="formCheck">
+                        <label for="formCheck" class="form-check-label text-secondary"><small>Remember Me</small></label>
+                    </div>
+        </form> 
+                    
+                </div>
+
+                <div class="input-group mb-3">
+                    <button type ="button" onclick="location.href='login.php'" class="btn btn-lg btn-primary w-100 fs-6" >Register</button>
+                </div>
+                
+                <body>
+    
+
+<?php
+    include "db.php";
+    
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        $username = mysqli_real_escape_string($conn, $_POST['username']);
+        $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+    
+        $sql = "INSERT INTO users_reg (username, password) VALUES ('$username', '$password')";
+    
+        if (mysqli_query($conn, $sql)) {
+            echo "New record created successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        }
+    }
+?>
---
+++ lab3sdt/student_list.php
@@ -0,0 +1,122 @@
+<?php
+    session_start();
+    if(!isset($_SESSION['username'])){
+        header("Location: login.php");
+        exit();
+    }
+?>
+
+<html>
+    <head>
+        <title>Student List</title>
+        
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" 
+    rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+  <body style="background-color: var(--bs-light-cyan);">
+  
+      <img src="fcutm.png" alt=logo width=315px height=65px>
+        <br>
+
+      
+
+       <!-- toggle button for mobile nav -->
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-nav" aria-controls="main-nav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+       <!-- navbar links -->
+       <nav class="navbar navbar-expand-lg navbar-dark bg-dark"> 
+  <div class="container-fluid">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="homepage.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="student_list.php">Student List</a>
+        </li>
+        <li class="nav-item">
+           <a class="nav-link" href="register.php">Add Students</a>
+        </li>
+        <li class="nav-item">
+           <a class="nav-link" href="logout.php">Logout</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-light" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+    </div>
+      
+    </nav>
+
+    <br>
+    <h2>Student List</h2>
+    <table border="1" class="table table-hover text-center">
+      <thead>
+        <tr class="table-dark text-uppercase text-white">
+            <td><b>ID</b></td>
+            <td><b>Name</b></td>
+            <td><b>Email</b></td>
+            <td><b>Phone</b></td>
+            <td><b>Gender</b></td>
+            <td><b>Course</b></td>
+            <td><b>Matric ID</b></td>
+            <td><b>Elective</b></td>
+            <td><b>Edit</b></td>
+            <td><b>Delete</b></td>
+        </tr>
+      </thead>
+        
+<?php
+include "db.php"; // Using database connection file here
+
+$sql = "SELECT * FROM users";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {    
+        echo "<tr>";
+        echo "<td>" . $row['id'] . "</td>";
+        echo "<td>" . $row['name'] . "</td>";
+        echo "<td>" . $row['email'] . "</td>";
+        echo "<td>" . $row['phone'] . "</td>";
+        echo "<td>" . $row['gender'] . "</td>";
+        echo "<td>" . $row['course'] . "</td>";
+        echo "<td>" . $row['matric_id'] . "</td>";
+        echo "<td>" . $row['elective'] . "</td>";
+        echo "<td> 
+                <form action='edit_student.php' method='GET'>
+                   <input type='hidden' name='id' value='" . $row['id'] . "'>
+                   <button type='submit' class='btn btn-primary'>Edit</button>
+                   </form>
+              </td>"; 
+        echo "<td>  
+                <form action='delete.php' method='GET'>
+                   <input type='hidden' name='id' value='" . $row['id'] . "'>
+                   <button type='submit' class='btn btn-danger'>Delete</button>
+                   </form>
+              
+          </td>";
+        echo "</tr>";
+    }
+}
+        ?>
+        </table>
+        <br><br>
+        
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
+
+
+
+
+
+      
